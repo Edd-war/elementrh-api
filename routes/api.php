ed3637d6114b481457a62233d5425816755ae803
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-06-14 21:56:34 -0600

    Generando EndPoint para Employees API

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,5 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::apiResource('companies', CompanyController::class);
 
 Route::apiResource('positions', PositionController::class);
+
+Route::apiResource('employees', EmployeeController::class);
