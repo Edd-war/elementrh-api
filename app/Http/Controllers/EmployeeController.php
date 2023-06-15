1db190f5f0d75713068f325dee8ac96b504e6129
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-06-14 21:33:55 -0600

    Método para mostrar todos los empleados
    También se repara el nombre del nombre de los recursos de Employee

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,17 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use App\Models\Employee;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Resources\Employee\EmployeeCollection;
 
 class EmployeeController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
         //
+        try
+        {
+            return response()->json([
+                'employees' => new EmployeeCollection(Employee::all()),
+            ], JsonResponse::HTTP_OK);
+        }
+        catch (Exception $e)
+        {
+            return response()->json([
+                'message' => 'Error al obtener los empleados',
+                'error' => $e->getMessage(),
+            ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
