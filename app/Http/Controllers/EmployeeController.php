adc9af7162294bdc46b56b793e87efc5eb462818
Eduardo Arroyo Morales <digiespirit.1807@gmail.com>
2023-06-14 21:50:38 -0600

    Método para crear un empleado

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -6,6 +6,7 @@ use Exception;
 use App\Models\Employee;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use App\Http\Requests\Employee\EmployeeRequest;
 use App\Http\Resources\Employee\EmployeeCollection;
 
 class EmployeeController extends Controller
@@ -34,9 +35,31 @@ class EmployeeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(EmployeeRequest $employeeRequest): JsonResponse
     {
         //
+        try
+        {
+            $employeeRequest->validated();
+
+            $employee = new Employee();
+            $employee->name = $employeeRequest->name;
+            $employee->company_id = $employeeRequest->company_id;
+            $employee->position_id = $employeeRequest->position_id;
+            $employee->save();
+            
+            return response()->json([
+                'message' => 'Empleado creado exitosamente',
+                'employee' => $employee,
+            ], JsonResponse::HTTP_CREATED);
+        }
+        catch (Exception $e)
+        {
+            return response()->json([
+                'message' => 'Error al crear el empleado',
+                'error' => $e->getMessage(),
+            ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
     /**
